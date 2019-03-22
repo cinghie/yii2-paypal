2fb46b2271414c371384a69d1ae7526d04e88445
Giandomenico Olini <giando.olini@gogodigital.it>
2019-03-22 13:16:08 +0100

    Fix bug

--- components/Paypal.php
+++ components/Paypal.php
@@ -68,27 +68,27 @@ class Paypal extends Component
 	 */
 	public function init()
 	{
-		if(!isset($this->config['mode'])) {
+		if( !isset($this->config['mode']) ) {
 			$this->config['mode'] = 'sandbox';
 		}
 
-		if(!isset($this->config['http.ConnectionTimeOut'])) {
+		if( !isset($this->config['http.ConnectionTimeOut']) ) {
 			$this->config['http.ConnectionTimeOut'] = 30;
 		}
 
-		if(!isset($this->config['http.Retry'])) {
+		if( !isset($this->config['http.Retry']) ) {
 			$this->config['http.Retry'] = 1;
 		}
 
-		if(!isset($this->config['log.LogEnabled'])) {
+		if( !isset($this->config['log.LogEnabled']) ) {
 			$this->config['log.LogEnabled'] = YII_DEBUG ? 1 : 0;
 		}
 
-		if(!isset($this->config['log.FileName'])) {
-			$this->config['log.FileName'] = '@runtime/logs/paypal.log';
+		if( !isset($this->config['log.FileName']) ) {
+			$this->config['log.FileName'] = Yii::getAlias('@runtime/logs/paypal.log');
 		}
 
-		if(!isset($this->config['log.LogLevel'])) {
+		if( !isset($this->config['log.LogLevel']) ) {
 			$this->config['log.LogLevel'] = 'ERROR';
 		}
 
@@ -101,11 +101,11 @@ class Paypal extends Component
 
 		$this->_apiContext->setConfig($this->config);
 
-		if(isset($this->config['log.FileName'], $this->config['log.LogEnabled']) && $this->config['log.LogEnabled'])
+		if( $this->config['log.LogEnabled'] && isset($this->config['log.FileName'], $this->config['log.LogEnabled']) )
 		{
-			$logFileName = \Yii::getAlias($this->config['log.FileName']);
+			$logFileName = $this->config['log.FileName'];
 
-			if (!file_exists($logFileName) && !touch($logFileName))
+			if ( !file_exists($logFileName) && !touch($logFileName) )
 			{
 				throw new ErrorException('Can\'t create paypal.log file at: ' . $logFileName);
 			}
