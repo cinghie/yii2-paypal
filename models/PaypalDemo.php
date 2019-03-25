a558772ddde7d24026ddcbffb592cdd5f5f1df9d
Giando <giando.olini@gogodigital.it>
2019-03-26 00:03:03 +0100

    Adding model PaypalDemo

---
+++ models/PaypalDemo.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-paypal
+ * @license BSD-3-Clause
+ * @package yii2-paypal
+ * @version 0.2.2
+ */
+
+namespace cinghie\paypal\models;
+
+use Yii;
+use PayPal\Api\Address;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\CreditCard;
+use PayPal\Api\FundingInstrument;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+use PayPal\Rest\ApiContext;
+use PayPal\Exception\PayPalConnectionException;
+use yii\base\ErrorException;
+
+/**
+ * Class PaypalDemo
+ */
+class PaypalDemo
+{
+	/** @var ApiContext $_apiContext */
+	private $_apiContext;
+
+	/**
+	 * PaypalDemo constructor.
+	 *
+	 * @throws ErrorException
+	 */
+	public function __construct()
+	{
+		$this->_apiContext = Yii::$app->paypal->getApiContext();
+
+		if(!Yii::$app->paypal->checkIsSandbox()) {
+			throw new ErrorException(Yii::t('paypal','Can\'t use Demo if Paypal is not in Sandbox mode'));
+		}
+	}
+
+	/**
+	 * Credit Card Payment Demo
+	 *
+	 * @return Payment
+	 */
+	public function payCreditCardDemo()
+	{
+		$addr = new Address();
+		$addr->setLine1('52 N Main ST');
+		$addr->setCity('Johnstown');
+		$addr->setCountryCode('US');
+		$addr->setPostalCode('43210');
+		$addr->setState('OH');
+
+		$card = new CreditCard();
+		$card->setNumber('4417119669820331');
+		$card->setType('visa');
+		$card->setExpireMonth('11');
+		$card->setExpireYear('2010');
+		$card->setCvv2('874');
+		$card->setFirstName('Joe');
+		$card->setLastName('Shopper');
+		$card->setBillingAddress($addr);
+
+		$fi = new FundingInstrument();
+		$fi->setCreditCard($card);
+
+		$payer = new Payer();
+		$payer->setPaymentMethod('credit_card');
+		$payer->setFundingInstruments(array($fi));
+
+		$amountDetails = new Details();
+		$amountDetails->setSubtotal('1.00');
+		$amountDetails->setTax('0.22');
+		$amountDetails->setShipping('0.10');
+
+		$amount = new Amount();
+		$amount->setCurrency('EUR');
+		$amount->setTotal('1.32');
+		$amount->setDetails($amountDetails);
+
+		$transaction = new Transaction();
+		$transaction->setAmount($amount);
+		$transaction->setDescription('This is the payment transaction description.');
+
+		$payment = new Payment();
+		$payment->setIntent('sale');
+		$payment->setPayer($payer);
+		$payment->setTransactions(array($transaction));
+
+		$paymentDemo = new Payment();
+
+		try {
+			$paymentDemo = $payment->create($this->_apiContext);
+		} catch (PayPalConnectionException $e) {
+			new PaypalError($e->getData());
+		} catch (Exception $ex) {
+			die(var_dump($ex));
+		}
+
+		return $paymentDemo;
+	}
+
+	/**
+	 * Paypal Payment Demo
+	 *
+	 * @return Payment
+	 */
+	public function payPaypalDemo()
+	{
+		$addr = new Address();
+		$addr->setLine1('52 N Main ST');
+		$addr->setCity('Johnstown');
+		$addr->setCountryCode('US');
+		$addr->setPostalCode('43210');
+		$addr->setState('OH');
+
+		$payer = new Payer();
+		$payer->setPaymentMethod('paypal');
+
+		$redirectUrls = new RedirectUrls();
+		$redirectUrls->setReturnUrl('https://www,google.com');
+		$redirectUrls->setCancelUrl('https://www,google.com');
+
+		$amountDetails = new Details();
+		$amountDetails->setSubtotal('1.00');
+		$amountDetails->setTax('0.22');
+		$amountDetails->setShipping('0.10');
+		$amount = new Amount();
+		$amount->setCurrency('EUR');
+		$amount->setTotal('1.32');
+		$amount->setDetails($amountDetails);
+
+		$transaction = new Transaction();
+		$transaction->setAmount($amount);
+		$transaction->setDescription('This is the payment transaction description.');
+
+		$payment = new Payment();
+		$payment->setIntent('sale');
+		$payment->setPayer($payer);
+		$payment->setRedirectUrls($redirectUrls);
+		$payment->setTransactions(array($transaction));
+
+		try {
+			$paymentDemo = $payment->create($this->_apiContext);
+			$approvalUrl = $paymentDemo->getApprovalLink();
+			echo	'<a href="'.$approvalUrl.'">Paga con PayPal</a>';
+		} catch (PayPalConnectionException $e) {
+			new PaypalError($e->getData());
+		} catch (Exception $ex) {
+			die(var_dump($ex));
+		}
+
+		return $paymentDemo;
+	}
+}
